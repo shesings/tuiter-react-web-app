9617b42fe8bbf037a65ea94d4952a9fc83b78d1d
Sheetal <sheetalsingh7007@gmail.com>
2022-10-11 18:52:40 -0500

    2.4.2 Boolean Variable

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -31,3 +31,27 @@ console.log(booleanVariable);
 console.log(isNumber);
 console.log(isString);
 console.log(isBoolean);
+
+// 2.4.2 Boolean Variables
+console.log('Boolean Variable');
+let true1 = true;
+let false1 = false;
+let false2 = true1 && false1;
+let true2 = true1 || false1;
+let true3 = !false2;
+let true4 = numberVariable === 123;
+let true5 = floatingPointNumber !== 321.432;
+let false3 = numberVariable < 100;
+let sortaTrue = '1' == 1;
+let notTrue = '1' === 1;
+
+console.log(true1);
+console.log(false1);
+console.log(false2);
+console.log(true2);
+console.log(true3);
+console.log(true4);
+console.log(true5);
+console.log(false3);
+console.log(sortaTrue);
+console.log(notTrue);
