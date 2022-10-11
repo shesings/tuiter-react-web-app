e58d1922eddb0e0e7e76f6be166204e5f2def917
Sheetal <sheetalsingh7007@gmail.com>
2022-10-11 18:25:07 -0500

    2.3 Writing to the Console

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -1,3 +1,14 @@
 // Commented out code below as mentioned in 2.2
 // for future sections of Lab
 // alert('Hello World!');
+console.log('Hello World!');
+console.log('Variables and Constants');
+global1 = 10;
+var functionScoped = 2;
+let blockScoped = 5;
+const constant1 = global1 + functionScoped - blockScoped;
+
+console.log(global1);
+console.log(functionScoped);
+console.log(blockScoped);
+console.log(constant1);
