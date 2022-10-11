f544d7dfed3b8632decdea10cc4d3aa9cd27ab38
Sheetal <sheetalsingh7007@gmail.com>
2022-10-11 18:30:24 -0500

    2.4.1 Variable Types

--- public/labs/a5/index.js
+++ public/labs/a5/index.js
@@ -2,6 +2,7 @@
 // for future sections of Lab
 // alert('Hello World!');
 console.log('Hello World!');
+// 2.4 Variables and Constants
 console.log('Variables and Constants');
 global1 = 10;
 var functionScoped = 2;
@@ -12,3 +13,21 @@ console.log(global1);
 console.log(functionScoped);
 console.log(blockScoped);
 console.log(constant1);
+
+// 2.4.1 Variable Types
+console.log('Variable Types');
+let numberVariable = 123;
+let floatingPointNumber = 234.345;
+let stringVariable = 'Hello World!';
+let booleanVariable = true;
+let isNumber = typeof numberVariable;
+let isString = typeof stringVariable;
+let isBoolean = typeof booleanVariable;
+
+console.log(numberVariable);
+console.log(floatingPointNumber);
+console.log(stringVariable);
+console.log(booleanVariable);
+console.log(isNumber);
+console.log(isString);
+console.log(isBoolean);
