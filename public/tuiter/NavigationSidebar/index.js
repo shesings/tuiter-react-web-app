efe84d9a45cc49e56b8c299ad45aee845e88e8bc
Sheetal <sheetalsingh7007@gmail.com>
2022-10-16 17:36:00 -0500

    4.2 completed with navigation sidebar

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -1,23 +1,24 @@
 const NavigationSidebar = (active) => {
+    // console.log(active);
     return (`
        <div class="list-group">
          <a class="list-group-item" href="#">
            <i class="fab fa-twitter"></i></a>     
          <a id="checkActiveHome" class="${active === 'home' ? 'active': ''} list-group-item" href="../HomeScreen/index.html">
             <i class="fa fa-home"></i> Home</a>   
-         <a class="list-group-item" class="${active === 'explore' ? 'active': ''} list-group-item" href="../ExploreScreen/index.html">
+         <a id="checkActiveHome" class="${active === 'explore' ? 'active': ''} list-group-item" href="../ExploreScreen/index.html">
            <i class="fa fa-hashtag"></i> Explore</a> 
-         <a class="list-group-item" href="#">
+         <a id="checkActiveHome" class="${active === 'notifications' ? 'active': ''} list-group-item" href="#">
            <i class="fa fa-bell"></i> Notifications</a>
-         <a class="list-group-item" href="#">
+         <a id="checkActiveHome" class="${active === 'messages' ? 'active': ''} list-group-item" href="#">
            <i class="fa fa-envelope"></i> Messages</a>
-         <a class="list-group-item" href="#">
+         <a id="checkActiveHome" class="${active === 'bookmarks' ? 'active': ''} list-group-item" href="#">
            <i class="fa fa-bookmark"></i> Bookmarks</a>    
-         <a class="list-group-item" href="#">
+         <a id="checkActiveHome" class="${active === 'lists' ? 'active': ''} list-group-item" href="#">
            <i class="fa fa-list"></i> Lists</a>  
-         <a class="list-group-item" href="#">
+         <a id="checkActiveHome" class="${active === 'profile' ? 'active': ''} list-group-item" href="#">
            <i class="fa fa-user"></i> Profile</a>  
-         <a class="list-group-item" href="#">
+         <a id="checkActiveHome" class="${active === 'more' ? 'active': ''} list-group-item" href="#">
            <span class="fa-stack small">
                 <i class="fa fa-circle fa-stack-2x stack-item"></i>
                 <i class="fa fa-ellipsis-h fa-stack-1x fa-inverse stack-item"></i>
