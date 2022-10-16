ba94e0eee7285c4e74cb4448a24d395c1bf4724b
Sheetal <sheetalsingh7007@gmail.com>
2022-10-15 22:54:08 -0500

    Working on functionality of challenge

--- public/tuiter/HomeScreen/index.js
+++ public/tuiter/HomeScreen/index.js
@@ -1,4 +1,4 @@
-// import NavigationSidebar from "../NavigationSidebar/index.js";
+import NavigationSidebar from "../NavigationSidebar/index.js";
 // import WhoToFollowList from "../WhoToFollowList/index.js";
 // import PostSummaryList from "../PostSummaryList/index.js";
 // import HomeComponent from "./HomeComponent.js";
@@ -6,8 +6,8 @@ function homeComponent() {
     $('#wd-explore').append(`
     <div class="row mt-2">
         <div class="col-2 col-md-2 col-lg-1 col-xl-2 bg-warning">
-            <h3>Navigation Sidebar</h3>
-            
+<!--            <h3>Navigation Sidebar</h3>-->
+            ${NavigationSidebar('home')}
         </div>
      
         <div class="col-10 col-lg-7 col-xl-6 bg-info">
@@ -18,6 +18,7 @@ function homeComponent() {
         <div class="d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4 bg-primary">
             <ul class="wd-suggested-list list-group">
             <h3>Post Summary List</h3>
+
 <!--                <li class="list-group-item wd-user-suggestion d-flex justify-content-between align-items-start list-title">Who to follow</li>-->
                 
                 
--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -1,33 +1,42 @@
-const NavigationSidebar = () => { return(`
-   <div class="list-group">
-     <a class="list-group-item" href="#">
-       <i class="fab fa-twitter"></i></a>
-     <a class="list-group-item" href="#">
-       <i class="fa fa-home"></i> Home</a> 
-     <a class="list-group-item" href="#">
-       <i class="fa fa-hashtag"></i> Explore</a> 
-     <a class="list-group-item" href="#">
-       <i class="fa fa-bell"></i> Notifications</a>
-     <a class="list-group-item" href="#">
-       <i class="fa fa-envelope"></i> Messages</a>
-     <a class="list-group-item" href="#">
-       <i class="fa fa-bookmark"></i> Bookmarks</a>    
-     <a class="list-group-item" href="#">
-       <i class="fa fa-list"></i> Lists</a>  
-     <a class="list-group-item" href="#">
-       <i class="fa fa-user"></i> Profile</a>  
-     <a class="list-group-item" href="#">
-       <span class="fa-stack small">
-            <i class="fa fa-circle fa-stack-2x stack-item"></i>
-            <i class="fa fa-ellipsis-h fa-stack-1x fa-inverse stack-item"></i>
-       </span>
-       More
-     </a>  
-   </div>
-   <div class="d-grid mt-2">
-     <a href="../tuit.html"
-        class="btn btn-primary btn-block rounded-pill">
-        Tweet</a>
-</div> `);
+const NavigationSidebar = (active) => {
+    if (active === 'home') {
+        // const addClassTest = $("#checkActiveHome");
+        // const addClassTest = document.querySelector(".list-group");
+        // const addClassTest = document.getElementById('checkActiveHome').className = 'active';
+        // console.log(addClassTest);
+        // addClassTest.addClass("active");
+    }
+    return (`
+       <div class="list-group">
+         <a class="list-group-item" href="#">
+           <i class="fab fa-twitter"></i></a>
+         <a id="checkActiveHome" class="list-group-item" href="#">
+           <i class="fa fa-home"></i> Home</a> 
+         <a class="list-group-item" href="#">
+           <i class="fa fa-hashtag"></i> Explore</a> 
+         <a class="list-group-item" href="#">
+           <i class="fa fa-bell"></i> Notifications</a>
+         <a class="list-group-item" href="#">
+           <i class="fa fa-envelope"></i> Messages</a>
+         <a class="list-group-item" href="#">
+           <i class="fa fa-bookmark"></i> Bookmarks</a>    
+         <a class="list-group-item" href="#">
+           <i class="fa fa-list"></i> Lists</a>  
+         <a class="list-group-item" href="#">
+           <i class="fa fa-user"></i> Profile</a>  
+         <a class="list-group-item" href="#">
+           <span class="fa-stack small">
+                <i class="fa fa-circle fa-stack-2x stack-item"></i>
+                <i class="fa fa-ellipsis-h fa-stack-1x fa-inverse stack-item"></i>
+           </span>
+           More
+         </a>  
+       </div>
+       <div class="d-grid mt-2">
+         <a href="../tuit.html"
+            class="btn btn-primary btn-block rounded-pill">
+            Tweet</a>
+       </div> 
+`);
 }
 export default NavigationSidebar;
