c780d5ed02b7691724b17dfe07ff7e8f8540363c
Sheetal <sheetalsingh7007@gmail.com>
2022-10-21 18:57:38 -0500

    2.11 Working with the HTML style attribute

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,7 +1,9 @@
 import Classes from "./classes";
+import Styles from "./styles";
 function Assignment6() { return (
     <div>
         <h1>Assignment 6</h1>
+        <Styles/>
         <Classes/>
     </div> );
 }
