feb07a0f7278691de6f6db7e564b8410d51ee6cc
Sheetal <sheetalsingh7007@gmail.com>
2022-10-21 21:40:29 -0500

    3.3 Re-implemting WhoToFollowList as a React component

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,22 +1,13 @@
 import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar/index.js";
-import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from "./who-to-follow-list/index.js";
 function Tuiter() {
     return(
     <div>
         <Nav/>
         <NavigationSidebar active="home"/>
-        <WhoToFollowListItem who={{
-            userName: 'NASA', handle: 'NASA',
-            avatarIcon: 'nasa-logo.png',
-        }}/>
-        <WhoToFollowListItem who={{
-            userName: 'Tesla', handle: 'tesla',
-            avatarIcon: 'tesla-logo.png', }}/>
-        <WhoToFollowListItem who={{
-            userName: 'SpaceX', handle: 'SpaceX',
-            avatarIcon: 'space-x-logo.jpeg', }}/>
         <h1>Tuiter</h1>
+        <WhoToFollowList/>
     </div>
     );
 }
