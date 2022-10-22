07afda37a5d8648b135974e37a0753f4b4158522
Sheetal <sheetalsingh7007@gmail.com>
2022-10-21 19:12:33 -0500

    2.12 Generating conditional output

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,8 +1,10 @@
 import Classes from "./classes";
 import Styles from "./styles";
+import ConditionalOutput from "./conditional-output";
 function Assignment6() { return (
     <div>
         <h1>Assignment 6</h1>
+        <ConditionalOutput/>
         <Styles/>
         <Classes/>
     </div> );
