e71d25b10896c1cb356de8cbcff90dee3e154ee2
Sheetal <sheetalsingh7007@gmail.com>
2022-10-21 22:46:15 -0500

    3.7 Implement the Tuiter React component

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,16 +1,21 @@
-import Nav from "../nav";
+import React from "react";
 import NavigationSidebar from "./navigation-sidebar/index.js";
 import WhoToFollowList from "./who-to-follow-list/index.js";
-import PostSummaryList from "./post-summary-list/index.js";
+import ExploreComponent from "./explore";
+
 function Tuiter() {
     return(
-    <div>
-        <Nav/>
-        <NavigationSidebar active="home"/>
-        <h1>Tuiter</h1>
-        <WhoToFollowList/>
-        <PostSummaryList/>
-    </div>
+        <div className="row mt-2">
+            <div className="col-2 col-md-2 col-lg-1 col-xl-2">
+                <NavigationSidebar active="explore"/>
+            </div>
+            <div className="col-10 col-md-10 col-lg-7 col-xl-6" style={{"position": "relative"}}>
+                <ExploreComponent/>
+            </div>
+            <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
+                <WhoToFollowList/>
+            </div>
+        </div>
     );
 }
 export default Tuiter
