ba8ce0dde403d6542e11501c413a3e7c1933e28a
Sheetal <sheetalsingh7007@gmail.com>
2022-10-21 21:01:08 -0500

    3.1.5 Render dynamic classes with embedded expressions

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,8 +1,10 @@
 import Nav from "../nav";
+import NavigationSidebar from "./navigation-sidebar/index.js";
 function Tuiter() {
     return(
     <div>
         <Nav/>
+        <NavigationSidebar active="home"/>
         <h1>Tuiter</h1>
     </div>
     );
