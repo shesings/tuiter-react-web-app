4f2aa2dd9a06ec1d835678b7e8fc9e4b8f478b2a
Sheetal <sheetalsingh7007@gmail.com>
2022-10-23 12:29:20 -0500

    2.1 and 2.2 Create an Assignment 7 component

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,9 +1,19 @@
 import Nav from "../nav";
 import Assignment6 from "./a6";
-function Labs() { return(
-    <div>
-        <Nav/>
-        <Assignment6/>
-    </div> );
+import Assignment7 from "./a7";
+import {Routes, Route} from "react-router";
+
+function Labs() {
+    return (
+        <div>
+            <Nav/>
+            <Routes>
+                <Route index
+                       element={<Assignment6/>}/>
+                <Route path="a7"
+                       element={<Assignment7/>}/>
+            </Routes>
+        </div>
+    );
 }
 export default Labs;
