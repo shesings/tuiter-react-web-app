0a56366617da634bc0ef84725b8f401e79f7d400
Sheetal <sheetalsingh7007@gmail.com>
2022-11-04 23:58:28 -0500

    3.1 Re-implement the WhoToFolowList component to use Redux

--- src/tuiter/who-to-follow-list/index.js
+++ src/tuiter/who-to-follow-list/index.js
@@ -1,14 +1,20 @@
 import React from "react";
-import whoArray from './who.json';
+// import whoArray from '../data/who.json';
 import WhoToFollowListItem from "./who-to-follow-list-item";
-const WhoToFollowList = () => { return(
-    <ul className="list-group">
-        <li className="list-group-item">
-            <h3>Who to follow</h3> </li>
-        {
-            whoArray.map(who =>
-                <WhoToFollowListItem key={who._id} who={who}/>
-            ) }
-    </ul> );
+
+import {useSelector} from "react-redux";
+
+const WhoToFollowList = () => {
+    const whoArray = useSelector((state) => state.who);
+    return(
+        <ul className="list-group">
+            <li className="list-group-item">
+                <h3>Who to follow</h3> </li>
+            {
+                whoArray.map(who =>
+                    <WhoToFollowListItem key={who._id} who={who}/>
+                ) }
+        </ul>
+    );
 };
 export default WhoToFollowList;
