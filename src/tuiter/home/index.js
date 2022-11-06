abc5f54939c7db4ef39fc4a67e5553d580e0fb66
Sheetal <sheetalsingh7007@gmail.com>
2022-11-06 14:21:40 -0500

    add 3.3.2

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -3,13 +3,16 @@ import React from "react";
 // import PostSummaryList from "../post-summary-list/index.js";
 // import PostList from "../post-list/index.js";
 // import WhoToFollowList from "../who-to-follow-list/index.js";
+import WhatsHappening from "./whats-happening.js";
 import TuitsList from "../tuits/index.js";
 
 import "./index.css";
 
 const HomeComponent = () => {
     return( <>
+    
             <div className="row mt-2">
+                <WhatsHappening/>
                 <div className="col-10 col-lg-7 col-xl-6">
                     {TuitsList()}
                 </div>
